177a045915f3eea5eeaaf6aed74ce7a0e1b151d8
mikael.deurell <mikael@deurell.net>
2019-01-30 07:56:43 +0100

    added imgui

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include "shader.h"
+
+
 
 void onResize(GLFWwindow* window, int height, int width);
 void processInput(GLFWwindow* window);
@@ -16,20 +22,21 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    GLFWwindow* window = glfwCreateWindow(1920, 1080, "gfxlab", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "gfxlab", nullptr, nullptr);
     if(window == nullptr) {
         std::cout << "failed to create window.";
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
 
     if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
         std::cout << "failed to init glad";
         return -1;
     }
 
-    glViewport(0, 0, 1920, 1080);
+    glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, onResize);
 
     Shader shader("vertex.shader", "fragment.shader");
@@ -54,25 +61,52 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(nullptr);
+    float my_color[] = {0.1f, 0.9f, 0.1f, 1.0f};
 
     while(!glfwWindowShouldClose(window)){
         processInput(window);
 
-        int width, height;
-        glfwGetFramebufferSize(window, &width, &height);
-        glViewport(0, 0, width, height);
-        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
 
+        ImGui::Begin("debug window");
+        ImGui::Text("FPS: %.1f", ImGui::GetIO().Framerate);
+
+        ImGui::ColorEdit4("frag_col", my_color);
+        ImGui::End();
+
+        glClear(GL_COLOR_BUFFER_BIT);
         shader.setFloat("time", (float)glfwGetTime());
-        shader.setVec4f("col", 0.1f, 0.9f, 0.1f, 1.0f);
+        shader.setVec4f("col", my_color[0], my_color[1], my_color[2], my_color[4]);
         shader.use();
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        glViewport(0, 0, width, height);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
     glfwTerminate();
     return 0;
 }

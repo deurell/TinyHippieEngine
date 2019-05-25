77c9d4310dd12c44d79c0a3a868bf3b40e048135
mikael.deurell <mikael@deurell.net>
2019-05-25 23:09:24 +0200

    multiple samplers

--- Shaders/fragment.shader
+++ Shaders/fragment.shader
@@ -1,20 +1,12 @@
 #version 330 core
 out vec4 FragColor;
 
+in vec3 ourColor;
 in vec2 TexCoord;
 
-uniform float time;
-uniform vec4 col;
+uniform sampler2D texture1;
+uniform sampler2D texture2;
 
-uniform sampler2D ourTexture;
-
-void main(){
-    float pi = 3.1415926535;
-
-    vec4 col2 = abs(sin(time)) * col;
-    //FragColor = col2;
-    float u = TexCoord.x + sin(0.7*time);
-    float v = TexCoord.y + sin(0.5*time+pi/2);
-
-    FragColor = texture(ourTexture, vec2(u,v)) * col2;
+void main() {
+    FragColor = mix(texture(texture1,TexCoord),texture(texture2, TexCoord),0.5);
 }
--- main.cpp
+++ main.cpp
@@ -1,7 +1,3 @@
-#include "imgui/imgui.h"
-#include "imgui/imgui_impl_glfw.h"
-#include "imgui/imgui_impl_opengl3.h"
-
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
@@ -14,7 +10,6 @@
 
 void onResize(GLFWwindow* window, int height, int width);
 void processInput(GLFWwindow* window);
-std::string loadShader(const std::string &path);
 
 int main() {
     glfwInit();
@@ -50,89 +45,86 @@ int main() {
     Shader shader("vertex.shader", "fragment.shader");
 
     float vertices[] = {
-            // vertex               // uv_coord
-            -1.0f, -1.0f, 0.0f,     0.0f, 0.0f,
-            1.0f, -1.0f, 0.0f,      1.0f, 0.0f,
-            -1.0f,  1.0f, 0.0f,     0.0f, 1.0f,
+            // positions          // colors           // texture coords
+            0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+            0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+            -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+            -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
+    };
 
-            -1.0f, 1.0f, 0.0f,      0.0f, 1.0f,
-            1.0f, 1.0f, 0.0f,       1.0f, 1.0f,
-            1.0f, -1.0f, 0.0f,      1.0f, 0.0f
-            };
+    unsigned int indices[] = {
+            0, 1, 3,
+            1, 2, 3
+    };
 
-    const int stride = 5;
-    const int verticesCnt = sizeof(vertices)/sizeof(float)/stride;
-
-    unsigned int VBO, VAO;
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride*sizeof(float), (void*)0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8* sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, stride*sizeof(float), (void*)(3*sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8* sizeof(float), (void*)(3*sizeof(float)));
     glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8* sizeof(float), (void*)(6*sizeof(float)));
+    glEnableVertexAttribArray(2);
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-
-    unsigned int texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-
+    unsigned int texture1, texture2;
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-    int width, height, channelCount;
-    unsigned char *data = stbi_load("micro.png", &width, &height, &channelCount, 0);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    int height, width, channels;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char* data = stbi_load("micro.png", &width, &height, &channels,0);
     if (data) {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
-    } else {
-        std::cout << "failed to load texture";
     }
     stbi_image_free(data);
 
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    data = stbi_load("zebra.jpg", &width, &height, &channels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    stbi_image_free(data);
 
-    ImGui::StyleColorsDark();
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init(glsl_version);
-    float my_color[] = {1.0f, 1.0f, 1.0f, 1.0f};
+    shader.use();
+    shader.setInt("texture1", 0);
+    shader.setInt("texture2", 1);
 
     double last_time = glfwGetTime();
     while(!glfwWindowShouldClose(window)){
         processInput(window);
 
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
-
-        ImGui::Begin("shader stats");
-        ImGui::Text("FPS: %.1f", ImGui::GetIO().Framerate);
-        ImGui::Text("Vertices: %i", verticesCnt);
-        ImGui::ColorEdit4("frag_col", my_color);
-        ImGui::End();
-
+        glClearColor(0.3,0.3,0.3,1);
         glClear(GL_COLOR_BUFFER_BIT);
-        shader.setFloat("time", (float)glfwGetTime());
-        shader.setVec4f("col", my_color[0], my_color[1], my_color[2], my_color[3]);
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+
         shader.use();
-
-        glBindTexture(GL_TEXTURE_2D, texture);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, verticesCnt);
-
-        ImGui::Render();
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         int frameWidth, frameHeight;
         glfwGetFramebufferSize(window, &frameWidth, &frameHeight);
@@ -143,10 +135,6 @@ int main() {
         while (glfwGetTime() < last_time + 1./60) {}
         last_time = glfwGetTime();
     }
-
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
     glfwTerminate();
     return 0;
 }

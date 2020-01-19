4ffe05fb02c0e1c3b5dda313312b762665e58058
Mikael Deurell <mikael@deurell.net>
2020-01-19 16:33:44 +0100

    handle both plain images and basis textures

--- texture.h
+++ texture.h
@@ -13,7 +13,7 @@ class Texture {
 public:
   explicit Texture(const std::string &imagePath,
                    basist::etc1_global_selector_codebook &codeBook,
-                   GLint format = GL_RGB, bool flipImage = true)
+                   GLint format = GL_RGB)
       : mId(0) {
 
     glGenTextures(1, &mId);
@@ -95,6 +95,29 @@ public:
 
       glGenerateMipmap(GL_TEXTURE_2D);
     }
+  }
+
+  explicit Texture(const std::string &imagePath, GLint format = GL_RGB,
+                   bool flipImage = true)
+      : mId(0) {
+
+    glGenTextures(1, &mId);
+    glBindTexture(GL_TEXTURE_2D, mId);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    int height, width, channels;
+    stbi_set_flip_vertically_on_load(flipImage);
+    const char *path = imagePath.c_str();
+    unsigned char *data = stbi_load(path, &width, &height, &channels, 0);
+    if (data) {
+      glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, (GLenum)format,
+                   GL_UNSIGNED_BYTE, data);
+      glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    stbi_image_free(data);
   }
 
   ~Texture() = default;

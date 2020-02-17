76733531fedaae905048dacd531c49b8b68da082
Mikael Deurell <mikael@deurell.net>
2020-02-17 18:06:18 +0100

    model loading v1.0

--- app.cpp
+++ app.cpp
@@ -11,8 +11,6 @@
 #include <glm/gtx/euler_angles.hpp>
 #include <iostream>
 
-#define STB_IMAGE_IMPLEMENTATION
-
 void renderLoopCallback(void *arg) { static_cast<App *>(arg)->render(); }
 
 void App::init() {
@@ -22,10 +20,6 @@ void App::init() {
 
 int App::run() {
   init();
-
-  Assimp::Importer importer;
-  const aiScene *scene = importer.ReadFile(
-      "Resources/nanosuit.obj", aiProcess_Triangulate | aiProcess_FlipUVs);
 
 #ifdef __APPLE__
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -57,8 +51,8 @@ int App::run() {
 
   glViewport(0, 0, screen_width, screen_height);
 
-  mTexture =
-      std::make_unique<Texture>("Resources/sup.basis", *mCodebook, GL_RGB);
+  // mTexture =
+  //    std::make_unique<Texture>("Resources/sup.basis", *mCodebook, GL_RGB);
 
 #ifdef Emscripten
   std::string glslVersionString = "#version 300 es\n";
@@ -70,7 +64,9 @@ int App::run() {
       "Shaders/lamp.vert", "Shaders/lamp.frag", glslVersionString);
 
   mLightingShader = std::make_unique<Shader>(
-      "Shaders/light.vert", "Shaders/light.frag", glslVersionString);
+      "Shaders/model.vert", "Shaders/model.frag", glslVersionString);
+
+  mModel = std::make_unique<Model>("Resources/nanosuit.obj");
 
   float cubeVertices[] = {
       -0.5f, -0.5f, -0.5f, 0.0f,  0.0f,  -1.0f, 0.0f,  0.0f,  0.5f,  -0.5f,
@@ -126,7 +122,6 @@ int App::run() {
   glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
                         (void *)(6 * sizeof(float)));
   glEnableVertexAttribArray(2);
-
   glBindVertexArray(0);
 
   // lamp
@@ -186,14 +181,13 @@ void App::render() {
   ImGui::Text("iTime: %.1f", ImGui::GetTime());
   ImGui::InputFloat3("mod", &mModelTranslate.x);
   ImGui::InputFloat3("cam", &mCamera->mPosition.x);
-  ImGui::InputFloat3("light", &mLightPos.x);
-  ImGui::Text("tex");
-  ImGui::Image((ImTextureID)mTexture->mId, ImVec2(100, 100));
+  ImGui::InputFloat3("light", &mPointLightPositions[0].x);
+  ImGui::InputFloat3("light2", &mPointLightPositions[1].x);
+
   ImGui::End();
 
   mLightingShader->use();
   mLightingShader->setVec3f("material.specular", 0.5f, 0.5f, 0.5f);
-  mLightingShader->setInt("material.diffuse", 0);
   mLightingShader->setFloat("material.shininess", 32.0f);
   // mLightingShader->setVec3f("material.ambientFallback", 0.3f, 0.2f, 0.5f);
   // mLightingShader->setVec3f("material.diffuseFallback", 0.2f, 0.5f, 0.8f);
@@ -219,49 +213,44 @@ void App::render() {
   mLightingShader->setFloat("pointLights[1].linear", 0.09);
   mLightingShader->setFloat("pointLights[1].quadratic", 0.032);
 
-  mLightingShader->setVec3f("viewPos", mCamera->mPosition);
+  mPointLightPositions[0].x = 2.0 * glm::sin(glfwGetTime());
+  mPointLightPositions[0].z = 1.0 - 1.0 * glm::sin(-1.5 * glfwGetTime());
+  mPointLightPositions[0].y = 2.0 * glm::sin(-0.5 * glfwGetTime());
+
+  mPointLightPositions[1].x = -1.0 * glm::sin(0.6 * glfwGetTime());
+  mPointLightPositions[1].z = 2.0 - 1.0 * glm::sin(1.7 * glfwGetTime());
+  mPointLightPositions[1].y = 1.0 - 0.5 * glm::sin(-1.2 * glfwGetTime());
+
+  // view/projection transformations
   glm::mat4 projection =
       mCamera->getPerspectiveTransform(45.0, screen_width / screen_height);
+
   glm::mat4 view = mCamera->getViewMatrix();
   mLightingShader->setMat4f("projection", projection);
   mLightingShader->setMat4f("view", view);
 
-  glActiveTexture(GL_TEXTURE0);
-  glBindTexture(GL_TEXTURE_2D, mTexture->mId);
+  // render the loaded model
+  glm::mat4 m2 = glm::mat4(1.0f);
+  m2 = glm::translate(m2, mModelTranslate);
+  m2 = glm::scale(m2, glm::vec3(0.2f, 0.2f, 0.2f));
+  m2 = glm::rotate(m2, (float)glfwGetTime(), glm::vec3(0.0, 1.0, 0.0));
+  mLightingShader->setMat4f("model", m2);
+  mModel->Draw(*mLightingShader);
 
   glBindVertexArray(mCubeVAO);
-
-  glm::vec3 cubePositions[] = {
-      glm::vec3(0.0f, 0.0f, 0.0f),    glm::vec3(2.0f, 5.0f, -15.0f),
-      glm::vec3(-1.5f, -2.2f, -2.5f), glm::vec3(-3.8f, -2.0f, -12.3f),
-      glm::vec3(2.4f, -0.4f, -3.5f),  glm::vec3(-1.7f, 3.0f, -7.5f),
-      glm::vec3(1.3f, -2.0f, -2.5f),  glm::vec3(1.5f, 2.0f, -2.5f)};
-
-  for (unsigned int i = 0; i < 8; i++) {
-    glm::mat4 model = glm::mat4(1.0f);
-    model = glm::translate(model, cubePositions[i]);
-    float angle = 32.0f * i;
-    model =
-        glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-
-    mLightingShader->setMat4f("model", model);
-
-    glDrawArrays(GL_TRIANGLES, 0, 36);
-  }
-
   mLampShader->use();
   mLampShader->setMat4f("projection", projection);
   mLampShader->setMat4f("view", view);
-  glm::mat4 model = glm::mat4(1.0);
-  model = glm::translate(model, mLightPos);
-  model = glm::scale(model, glm::vec3(0.2f));
-  mLampShader->setMat4f("model", model);
+  glm::mat4 mod = glm::mat4(1.0);
+  mod = glm::translate(mod, mLightPos);
+  mod = glm::scale(mod, glm::vec3(0.2f));
+  mLampShader->setMat4f("model", mod);
   glBindVertexArray(mLightVAO);
   for (unsigned int i = 0; i < point_light_count; i++) {
-    model = glm::mat4(1.0f);
-    model = glm::translate(model, mPointLightPositions[i]);
-    model = glm::scale(model, glm::vec3(0.2f));
-    mLampShader->setMat4f("model", model);
+    mod = glm::mat4(1.0f);
+    mod = glm::translate(mod, mPointLightPositions[i]);
+    mod = glm::scale(mod, glm::vec3(0.2f));
+    mLampShader->setMat4f("model", mod);
     glDrawArrays(GL_TRIANGLES, 0, 36);
   }
   ImGui::Render();
@@ -291,7 +280,7 @@ void App::processInput(GLFWwindow *window) {
 }
 
 void App::basisInit() {
-  basist::basisu_transcoder_init();
-  mCodebook = std::make_unique<basist::etc1_global_selector_codebook>(
-      basist::g_global_selector_cb_size, basist::g_global_selector_cb);
+  // basist::basisu_transcoder_init();
+  // mCodebook = std::make_unique<basist::etc1_global_selector_codebook>(
+  // basist::g_global_selector_cb_size, basist::g_global_selector_cb);
 }
--- include/app.h
+++ include/app.h
@@ -1,6 +1,8 @@
+#pragma once
+
 #include "camera.h"
+#include "model.h"
 #include "shader.h"
-#include "texture.h"
 #include <GLFW/glfw3.h>
 
 #ifdef Emscripten
@@ -24,11 +26,12 @@ private:
   void basisInit();
 
   GLFWwindow *mWindow;
-  std::unique_ptr<Texture> mTexture;
+  // std::unique_ptr<Texture> mTexture;
   std::unique_ptr<Shader> mLightingShader;
   std::unique_ptr<Shader> mLampShader;
   std::unique_ptr<Camera> mCamera;
-  std::unique_ptr<basist::etc1_global_selector_codebook> mCodebook;
+  std::unique_ptr<Model> mModel;
+  // std::unique_ptr<basist::etc1_global_selector_codebook> mCodebook;
 
   unsigned int mLightVAO;
   unsigned int mCubeVAO;
@@ -38,7 +41,7 @@ private:
 
   glm::vec3 mLightPos = {1.2f, 1.0f, 2.0f};
 
-  static constexpr int point_light_count = 3;
+  static constexpr int point_light_count = 2;
   glm::vec3 mPointLightPositions[point_light_count] = {
       glm::vec3(0.7f, 0.2f, 2.0f), glm::vec3(2.3f, -3.3f, -4.0f)};
 
@@ -48,6 +51,6 @@ private:
   float mXDeg = 0.0f;
   float mYDeg = 0.0f;
 
-  glm::vec3 mModelTranslate;
+  glm::vec3 mModelTranslate = {0.0f, -1.75f, 0.0f};
   glm::vec3 mObjectColor = {0.3, 0.8, 0.3};
 };

9a6bde17d610b5ef6d2cead92df812f1ee9b9d88
Mikael Deurell <mikael@deurell.net>
2021-02-07 00:36:49 +0100

    small fix for camera

--- source/camera.h
+++ source/camera.h
@@ -7,12 +7,12 @@ class Camera {
 public:
   Camera() = default;
 
-  Camera(const glm::vec3 position) : mPosition(position), mOrientation(glm::mat4(1)) {}
+  explicit Camera(const glm::vec3 position) : mPosition(position), mOrientation(glm::mat4(1)) {}
 
   Camera(const glm::vec3 position, const glm::quat orientation)
       : mPosition(position), mOrientation(orientation) {}
 
-  glm::mat4 getViewMatrix() {
+  [[nodiscard]] glm::mat4 getViewMatrix() const {
     return glm::translate(glm::mat4_cast(mOrientation), -mPosition);
   }
 
@@ -39,15 +39,15 @@ public:
   void pitch(float angle) { rotate(angle, 1.0f, 0.0f, 0.0f); }
   void roll(float angle) { rotate(angle, 0.0f, 0.0f, 1.0f); }
 
-  glm::mat4 getPerspectiveTransform(float fov, float aspect) {
+  static glm::mat4 getPerspectiveTransform(float fov, float aspect) {
     return glm::perspective(glm::radians(fov), aspect, 0.1f, 100.0f);
   }
 
-  glm::mat4 getOrtoTransform(float left, float right, float bottom, float up) {
+  static glm::mat4 getOrtoTransform(float left, float right, float bottom, float up) {
     return glm::ortho(left, right, bottom, up, 0.0f, 100.0f);
   }
 
   glm::vec3 mPosition;
   glm::quat mOrientation;
 };
-}; // namespace DL
+} // namespace DL

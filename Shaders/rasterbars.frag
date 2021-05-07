1d3d15382ab460ec78265ebbd733b888bef896ce
Mikael Deurell <mikael@deurell.net>
2021-05-07 11:38:50 +0200

    init it!

--- Shaders/rasterbars.frag
+++ Shaders/rasterbars.frag
@@ -7,15 +7,15 @@ out vec4 FragColor;
 #define PI 3.1415926535
 
 float addRasterbar(float old, float offset) {
-  float r1 = (1.0 - TexCoord.y + sin(iTime*2.0+offset)/2.5) * PI;
-  float col = pow(sin(r1),64.0);
+  float r1 = (1.0 - TexCoord.y + sin(iTime * 2.0 + offset) / 2.5) * PI;
+  float col = pow(sin(r1), 64.0);
   return col > 0.2 ? col : old;
 }
 void main() {
 
-  float col;
-  for (int i=0; i<8; i++) {
-    col = addRasterbar(col*0.9, 0.4* float(i));
+  float col = 0;
+  for (int i = 0; i < 6; i++) {
+    col = addRasterbar(col * 0.9, 0.4 * float(i));
   }
-  FragColor = vec4(0.5*col,0.3*col,col, 1.0);
+  FragColor = vec4(0.5 * col, 0.3 * col, col, 1.0);
 }
--- include/plane.h
+++ include/plane.h
@@ -6,26 +6,28 @@
 #include "shader.h"
 #include <camera.h>
 #include <glad/glad.h>
+#include <memory>
 #include <string_view>
 
 namespace DL {
 
 class Plane {
 public:
-  Plane(std::string_view vertexShader, std::string_view fragShader, std::string_view glslVersion, DL::Camera& camera);
+  Plane(std::string_view vertexShader, std::string_view fragShader,
+        std::string_view glslVersion, DL::Camera &camera);
   ~Plane() = default;
 
   void render(float delta) const;
 
-  glm::vec3 mPosition {0,0,0};
-  glm::vec3 mScale {1.0,1.0,1.0};
+  glm::vec3 mPosition{0, 0, 0};
+  glm::vec3 mScale{1.0, 1.0, 1.0};
   GLuint mVAO = 0;
   GLuint mVBO = 0;
   GLuint mEBO = 0;
 
 private:
   std::unique_ptr<DL::Shader> mShader;
-  DL::Camera& mCamera;
+  DL::Camera &mCamera;
 };
 
 } // namespace DL

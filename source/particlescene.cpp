5e72cbe4284cd4a5c19e962b174b34aacd0bc5ef
Mikael Deurell <mikael@deurell.net>
2022-12-17 14:44:40 +0100

    small fix

--- source/particlescene.cpp
+++ source/particlescene.cpp
@@ -21,8 +21,8 @@ void ParticleScene::init() {
 }
 void ParticleScene::initParticles() {
   for (int i = 0; i < number_of_particles; ++i) {
-    auto particle = std::make_unique<DL::Particle>(
-        *mPlanes[i], 0.9, glm::vec3(0, -32.0f, 0));
+    auto particle = std::make_unique<DL::Particle>(*mPlanes[i], 0.9,
+                                                   glm::vec3(0, -32.0f, 0));
     mParticles.emplace_back(std::move(particle));
   }
 }
@@ -39,10 +39,9 @@ void ParticleScene::initPlanes() {
     std::random_device rd;
     std::mt19937 mt(rd());
     std::uniform_real_distribution<double> dist(-2.0, 2.0);
-    glm::vec3 randomAxis = glm::vec3(dist(mt), dist(mt), dist(mt)*0.2);
+    glm::vec3 randomAxis = glm::vec3(dist(mt), dist(mt), dist(mt) * 0.2);
     plane->rotationAxis = glm::normalize(randomAxis);
-    plane->rotationSpeed = dist(mt) * glm::pi<float>()/180 * 360;
-
+    plane->rotationSpeed = dist(mt) * glm::pi<float>() / 180 * 360;
     mPlanes.emplace_back(std::move(plane));
   }
 }
@@ -71,8 +70,9 @@ void ParticleScene::onClick(double x, double y) {
   std::cout << "Mouse click @ x:" << x << " y:" << y << std::endl;
   glm::mat4 perspective = mCamera->getPerspectiveTransform();
   glm::vec4 viewport = glm::vec4(0, 0, 1024, 768);
-  glm::vec3 screenPos = glm::vec3(x, 768-y, 1);
-  glm::vec3 worldPos = glm::unProject(screenPos, mCamera->getViewMatrix(), perspective, viewport);
+  glm::vec3 screenPos = glm::vec3(x, 768 - y, 1);
+  glm::vec3 worldPos = glm::unProject(screenPos, mCamera->getViewMatrix(),
+                                      perspective, viewport);
   worldPos.z = 0;
 
   mParticleSystem->explode(worldPos);

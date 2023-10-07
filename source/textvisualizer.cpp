b62456379c0aff2805459fdff6aba0d9f84c9d78
Mikael Deurell <mikael@deurell.net>
2023-10-07 15:26:31 +0200

    easier text impl

--- source/textvisualizer.cpp
+++ source/textvisualizer.cpp
@@ -7,34 +7,28 @@ DL::TextVisualizer::TextVisualizer(std::string name, DL::Camera &camera,
                                    SceneNode &node, std::string_view text)
     : VisualizerBase(camera, std::move(name), std::string(glslVersionString),
                      "Shaders/status.vert", "Shaders/status.frag", node),
-      text_(text) {
+      text_(text),
+      shader_(std::make_unique<DL::Shader>(vertexShaderPath_, fragmentShaderPath_, glslVersionString_)) {
 
   camera.lookAt({0, 0, 0});
-  shader_ = std::make_unique<DL::Shader>(vertexShaderPath_, fragmentShaderPath_,
-                                         glslVersionString_);
-
   loadFontTexture("Resources/C64_Pro-STYLE.ttf");
   initGraphics();
 }
 
 void DL::TextVisualizer::render(const glm::mat4 &worldTransform, float delta) {
   shader_->use();
-  glm::mat4 model = glm::mat4(1.0f);
-  auto position = extractPosition(worldTransform);
-  auto rotation = extractRotation(worldTransform);
-  auto scale = extractScale(worldTransform);
 
-  model = glm::translate(model, position);
-  model = model * glm::mat4_cast(rotation);
-  model = glm::scale(model, glm::vec3(1.0, 1.0, 1.0) * scale);
+  glm::mat4 model = glm::translate(glm::mat4(1.0f), extractPosition(worldTransform));
+  model *= glm::mat4_cast(extractRotation(worldTransform));
+  model = glm::scale(model, extractScale(worldTransform));
 
+  glm::mat4 viewMatrix = camera_.getViewMatrix();
+  glm::mat4 perspectiveTransform = camera_.getPerspectiveTransform();
   shader_->setMat4f("model", model);
-  glm::mat4 view = camera_.getViewMatrix();
-  shader_->setMat4f("view", view);
-  glm::mat4 projectionMatrix = camera_.getPerspectiveTransform();
-  shader_->setMat4f("projection", projectionMatrix);
-
+  shader_->setMat4f("view", viewMatrix);
+  shader_->setMat4f("projection", perspectiveTransform);
   shader_->setFloat("iTime", static_cast<float>(glfwGetTime()));
+
   glActiveTexture(GL_TEXTURE0);
   glBindTexture(GL_TEXTURE_2D, fontTexture_);
   shader_->setInt("texture1", 0);
@@ -48,28 +42,25 @@ void DL::TextVisualizer::loadFontTexture(std::string_view fontPath) {
   iStream.seekg(0, iStream.end);
   const int size = iStream.tellg();
   iStream.seekg(0, iStream.beg);
-  char *fontData = new char[size];
 
-  iStream.read((char *)fontData, size);
+  std::unique_ptr<char[]> fontData = std::make_unique<char[]>(size);
+  iStream.read(fontData.get(), size);
   iStream.close();
 
-   stbtt_fontinfo fontInfo;
-    if (!stbtt_InitFont(&fontInfo, (unsigned char*)fontData, 0)) {
-        std::cout << "Failed to initialize font info." << std::endl;
-        return;
-    }
-    
-    // Determine the scale for the desired pixel height
-    fontScale_ = stbtt_ScaleForPixelHeight(&fontInfo, desiredPixelHeight_);
+  stbtt_fontinfo fontInfo;
+  if (!stbtt_InitFont(&fontInfo, (unsigned char *)fontData.get(), 0)) {
+    std::cout << "Failed to initialize font info." << std::endl;
+    return;
+  }
 
-     int ascent, descent, lineGap;
-    stbtt_GetFontVMetrics(&fontInfo, &ascent, &descent, &lineGap);
+  fontScale_ = stbtt_ScaleForPixelHeight(&fontInfo, desiredPixelHeight_);
 
-    // Convert to actual height using the scale.
-    float actualFontHeight = (ascent - descent + lineGap) * fontScale_;
+  int ascent, descent, lineGap;
+  stbtt_GetFontVMetrics(&fontInfo, &ascent, &descent, &lineGap);
 
-    fontSize_ = actualFontHeight;
+  float actualFontHeight = (ascent - descent + lineGap) * fontScale_;
 
+  fontSize_ = actualFontHeight;
 
   auto atlasData =
       std::make_unique<uint8_t[]>(fontAtlasWidth_ * FontAtlasHeight_);
@@ -85,7 +76,7 @@ void DL::TextVisualizer::loadFontTexture(std::string_view fontPath) {
   stbtt_PackSetOversampling(&context, fontOversampleX_, fontOversampleY_);
 
   if (!stbtt_PackFontRange(
-          &context, reinterpret_cast<const unsigned char *>(fontData), 0,
+          &context, reinterpret_cast<const unsigned char *>(fontData.get()), 0,
           fontSize_, fontFirstChar_, fontCharCount_, fontCharInfo_.get())) {
     std::cout << "pack font failed";
   }
@@ -106,7 +97,6 @@ void DL::TextVisualizer::loadFontTexture(std::string_view fontPath) {
                GL_RED, GL_UNSIGNED_BYTE, atlasData.get());
   glHint(GL_GENERATE_MIPMAP_HINT, GL_NICEST);
   glGenerateMipmap(GL_TEXTURE_2D);
-  delete[] fontData;
 }
 
 stbtt_packedchar *DL::TextVisualizer::getFontCharInfoPtr() {
@@ -120,58 +110,50 @@ DL::GlyphInfo DL::TextVisualizer::makeGlyphInfo(char character, float offsetX,
   stbtt_GetPackedQuad(getFontCharInfoPtr(), fontAtlasWidth_, FontAtlasHeight_,
                       chrRel, &offsetX, &offsetY, &quad, 1);
 
-  auto xmin = quad.x0;
-  auto xmax = quad.x1;
-  auto ymin = -quad.y1;
-  auto ymax = -quad.y0;
-  GlyphInfo info;
-  info.offsetX = offsetX;
-  info.offsetY = offsetY;
-  info.positions[0] = {xmin, ymin, 0};
-  info.positions[1] = {xmin, ymax, 0};
-  info.positions[2] = {xmax, ymax, 0};
-  info.positions[3] = {xmax, ymin, 0};
-  info.uvs[0] = {quad.s0, quad.t1};
-  info.uvs[1] = {quad.s0, quad.t0};
-  info.uvs[2] = {quad.s1, quad.t0};
-  info.uvs[3] = {quad.s1, quad.t1};
-  return info;
+  auto [xmin, xmax] = std::minmax({quad.x0, quad.x1});
+  auto [ymin, ymax] = std::minmax({-quad.y1, -quad.y0});
+
+  return GlyphInfo{
+      {{xmin, ymin, 0}, {xmin, ymax, 0}, {xmax, ymax, 0}, {xmax, ymin, 0}},
+      {{quad.s0, quad.t1},
+       {quad.s0, quad.t0},
+       {quad.s1, quad.t0},
+       {quad.s1, quad.t1}},
+      offsetX,
+      offsetY};
 }
 
 void DL::TextVisualizer::initGraphics() {
   std::vector<glm::vec3> vertices;
   std::vector<glm::vec2> uvs;
-  std::vector<uint16_t> indexes;
+  std::vector<uint16_t> indices;
 
-  uint16_t lastIndex = 0;
-  float offsetX = 0, offsetY = 0;
-  for (auto c : text_) {
+  uint16_t index = 0;
+  glm::vec2 offset(0.0f);
+  for (char c : text_) {
     if (c == '\n') {
-      offsetY += 42;
-      offsetX = 0;
+      offset.y += 42.0f;
+      offset.x = 0.0f;
       continue;
     }
 
-    const auto glyphInfo = makeGlyphInfo(c, offsetX, offsetY);
-    offsetX = glyphInfo.offsetX;
-    offsetY = glyphInfo.offsetY;
+    GlyphInfo glyphInfo = makeGlyphInfo(c, offset.x, offset.y);
+    offset.x = glyphInfo.offsetX;
+    offset.y = glyphInfo.offsetY;
 
-    vertices.emplace_back(glyphInfo.positions[0]);
-    vertices.emplace_back(glyphInfo.positions[1]);
-    vertices.emplace_back(glyphInfo.positions[2]);
-    vertices.emplace_back(glyphInfo.positions[3]);
-    uvs.emplace_back(glyphInfo.uvs[0]);
-    uvs.emplace_back(glyphInfo.uvs[1]);
-    uvs.emplace_back(glyphInfo.uvs[2]);
-    uvs.emplace_back(glyphInfo.uvs[3]);
-    indexes.push_back(lastIndex);
-    indexes.push_back(lastIndex + 1);
-    indexes.push_back(lastIndex + 2);
-    indexes.push_back(lastIndex);
-    indexes.push_back(lastIndex + 2);
-    indexes.push_back(lastIndex + 3);
+    for (int i = 0; i < 4; i++) {
+      vertices.push_back(glyphInfo.positions[i]);
+      uvs.push_back(glyphInfo.uvs[i]);
+    }
 
-    lastIndex += 4;
+    indices.push_back(index);
+    indices.push_back(index + 1);
+    indices.push_back(index + 2);
+    indices.push_back(index);
+    indices.push_back(index + 2);
+    indices.push_back(index + 3);
+
+    index += 4;
   }
 
   glGenVertexArrays(1, &VAO_);
@@ -183,6 +165,7 @@ void DL::TextVisualizer::initGraphics() {
                vertices.data(), GL_STATIC_DRAW);
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
   glEnableVertexAttribArray(0);
+
   glGenBuffers(1, &UVBuffer_);
   glBindBuffer(GL_ARRAY_BUFFER, UVBuffer_);
   glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2) * uvs.size(), uvs.data(),
@@ -190,11 +173,11 @@ void DL::TextVisualizer::initGraphics() {
   glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
   glEnableVertexAttribArray(1);
 
-  indexElementCount_ = indexes.size();
+  indexElementCount_ = indices.size();
   glGenBuffers(1, &indexBuffer_);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer_);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint16_t) * indexElementCount_,
-               indexes.data(), GL_STATIC_DRAW);
+               indices.data(), GL_STATIC_DRAW);
 
   glBindVertexArray(0);
 }
